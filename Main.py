9180dbe5284a9e9e7f7a1d80d2623f4e7b3783ae
PythonBotz <46303587+PythonBotz@users.noreply.github.com>
2019-01-01 13:59:25 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,4 +16,16 @@ async def python(ctx):
   """Tells you which python version you need"""
   await bot.say('you need python 3.6.5')
   
+def user_is_me(ctx):
+	return ctx.message.author.id == "277983178914922497"
+
+@bot.command(name='eval', pass_context=True, hidden=True)
+@commands.check(user_is_me)
+async def _eval(ctx, *, command):
+    res = eval(command)
+    if inspect.isawaitable(res):
+        await bot.say(await res)
+    else:
+    	await bot.say(res)
+  
 bot.run(os.environ['BOT_TOKEN'])
