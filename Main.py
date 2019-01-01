84655c2e104546c03b67d98378035ea97a3c2574
PythonBotz <46303587+PythonBotz@users.noreply.github.com>
2019-01-01 13:29:32 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,15 @@
+import discord
+from discord import commands
 
+@bot.event
+async def on_ready():
+  print('Logged in as')
+  print(bot.user.name)
+  print(bot.user.id)
+  
+@bot.command(pass_context=True)
+async def python(ctx):
+  """Tells you which python version you need"""
+  await bot.say('you need python 3.6.5')
+  
+bot.run(os.environ['BOT_TOKEN'])
