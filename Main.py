97806d45e08c85a16fb0776c661819d2d24295f4
PythonBotz <46303587+PythonBotz@users.noreply.github.com>
2019-01-01 13:45:25 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,15 +2,17 @@ import discord
 from discord.ext import commands
 import asyncio
 
-@client.event
+bot = commands.Bot(command_prefix=("p!"))
+
+@bot.event
 async def on_ready():
   print('Logged in as')
-  print(client.user.name)
-  print(client.user.id)
+  print(bot.user.name)
+  print(bot.user.id)
   
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def python(ctx):
   """Tells you which python version you need"""
-  await client.say('you need python 3.6.5')
+  await bot.say('you need python 3.6.5')
   
-client.run(os.environ['BOT_TOKEN'])
+bot.run(os.environ['BOT_TOKEN'])
