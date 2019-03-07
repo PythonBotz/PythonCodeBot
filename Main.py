de7298f7881c8a618c92c2b15e1cdd6088d4ddb9
PythonBotz <46303587+PythonBotz@users.noreply.github.com>
2019-03-06 17:10:18 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,7 +15,8 @@ async def on_ready():
 
 @bot.event
 async def on_server_join(server):
-	await bot.send_message(server.message.channel, "Hello my name is Python for more commands use p!help and more commands will be coming soon. :)")
+	channel = server.message.channel
+	await bot.send_message(channel, "Hello my name is Python for more commands use p!help and more commands will be coming soon. :)")
 
 @bot.event
 async def on_member_join(member):
