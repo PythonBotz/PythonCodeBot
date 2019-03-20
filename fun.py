f78550c3f85fcce22abbdf49f239054c4e37351f
PythonBotz <46303587+PythonBotz@users.noreply.github.com>
2019-03-19 23:15:45 -0700

    Update fun.py

--- fun.py
+++ fun.py
@@ -15,6 +15,9 @@ class Fun:
   async def python(self):
     await self.client.say("You need python 3.6.5 version")
     
+rank = {
+  "24":{"Major General"}
+    
   @commands.command(pass_context=True)
   async def ratings(self, ctx, user: str):
     url = "http://ratings.tankionline.com/get_stat/profile/?user={}&lang=en".format(user)
