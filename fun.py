a2c96238922b55c452fdaf10cd78440a7683da1e
PythonBotz <46303587+PythonBotz@users.noreply.github.com>
2019-03-19 20:07:58 -0700

    Update fun.py

--- fun.py
+++ fun.py
@@ -9,5 +9,11 @@ class Fun:
   async def ping(self):
     await self.client.say("Pong!")
     
+  @commands.command(pass_context=True)
+  async def userinfo(self, ctx user: discord.Member):
+    embed = discord.Embed(description=" ")
+    embed.add_field(name="Nickname", value="{}".format(ctx.message.author))
+    await self.client.say(embed=embed)
+    
 def setup(client):
   client.add_cog(Fun(client))
