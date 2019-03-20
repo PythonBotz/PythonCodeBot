2eb2339db7f8e0d81bed1a854e9d3dbd96dea495
PythonBotz <46303587+PythonBotz@users.noreply.github.com>
2019-03-19 20:14:09 -0700

    Update moderation.py

--- moderation.py
+++ moderation.py
@@ -1,2 +1,13 @@
 import discord
 from discord.ext import commands
+
+class Moderation:
+  def __init__(self, client):
+    self.client = client
+    
+  @commands.command()
+  async def userinfo(self):
+    await self.client.say("your nickname is noobperson")
+    
+def setup(client):
+  client.add_cog(Moderation(client))
