e43d61f5f5e08864010072db86349e00db81319f
PythonBotz <46303587+PythonBotz@users.noreply.github.com>
2019-03-19 18:29:17 -0700

    Update fun.py

--- fun.py
+++ fun.py
@@ -1,2 +1,13 @@
 import discord
 from discord.ext import commands
+
+class Fun:
+  def __init__(self, client):
+    self.client = bot
+    
+  @commands.command()
+  async def ping(self):
+    await self.bot.say("Pong!")
+    
+def setup(bot):
+  bot.add_cog(Fun(bot))
