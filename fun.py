b8ee0cf4aa4701e9f6e43ec8dffff9202418750d
PythonBotz <46303587+PythonBotz@users.noreply.github.com>
2019-05-10 19:38:08 -0700

    Update fun.py

--- fun.py
+++ fun.py
@@ -15,5 +15,9 @@ class Main:
   async def python(self):
     await self.client.say("You need python 3.6.5 version")
     
+  @commands.command(hidden=True)
+  async def baselink(self):
+    await self.client.say("https://discordapp.com/oauth2/authorize?&client_id=YOUR_CLIENTID_HERE&scope=bot&permissions=YOUR_VALUE_HERE")
+    
 def setup(client):
   client.add_cog(Main(client))
