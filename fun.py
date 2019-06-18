b4723b503b8fdd14e27adf11342b8818f54bf452
PythonBotz <46303587+PythonBotz@users.noreply.github.com>
2019-06-18 03:32:54 -0700

    Update fun.py

--- fun.py
+++ fun.py
@@ -23,12 +23,6 @@ class Main:
   async def download(self):
     """| download link for python 3.6.5"""
     await self.client.say("<https://www.python.org/downloads/release/python-365/>")
-  
-  @commands.command()
-  async def welcome(self):
-    embed = discord.Embed(title="👋 Nucleo | RJ just joined discord.py"), description="Welcome! to discord.py Nucleo | RJ! Enjoy your stay here!"), color=0x00ff00)
-    embed.add_field(name="Current Member Count", value="58")
-    await self.client.say(message.channel, embed=embed)
     
 def setup(client):
   client.add_cog(Main(client))
