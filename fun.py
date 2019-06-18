33dc1d9062cab5d2eaf202486db517f0de7a74af
PythonBotz <46303587+PythonBotz@users.noreply.github.com>
2019-06-18 03:23:46 -0700

    Update fun.py

--- fun.py
+++ fun.py
@@ -26,10 +26,10 @@ class Main:
   
   @commands.command()
   async def welcome(self):
-    embed = discord.Embed(title="👋 Nucleo | RJ just joined discord.py"), description="Welcome! to discord.py Nucleo | RJ! Enjoy your stay here!"), color=0x00ff00)
+    embed = discord.Embed(title="👋 Nucleo | RJ just joined discord.py")), description="Welcome! to discord.py Nucleo | RJ! Enjoy your stay here!")), color=0x00ff00)
     embed.set_thumbnail(url="https://images-ext-1.discordapp.net/external/rQcK0ZEeVhvlER973W2DG94YocL9bJuwx-vii-dNrus/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/403250141236166656/5ac09cf70179ad4d06aa2087edd8cd82.webp?format=png")
     embed.add_field(name="Current Member Count", value="58")
-    await self.client.send_message(message.channel, embed=embed)
+    await self.client.say(message.channel, embed=embed)
     
 def setup(client):
   client.add_cog(Main(client))
