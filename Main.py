b1601cca5f78dcd797e464fc234b87dcd3c581d7
PythonBotz <46303587+PythonBotz@users.noreply.github.com>
2019-08-15 04:09:40 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,7 +13,7 @@ async def on_ready():
 	print("Logged in as")
 	print(client.user.name)
 	print(client.user.id)
-	print(__discord__.version)
+	print(discord.__version__)
 	
 @client.event
 async def on_member_join(member):
